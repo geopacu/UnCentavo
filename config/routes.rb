ab7012c4218ea1c02698bcdcdbb8f1353314bc0b
Jorge Paez <george.paez@gmail.com>
2012-01-26 23:33:28 -0500

    links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Store::Application.routes.draw do
 
   match '/login',  :to => 'pages#login'
+  match '/account',  :to => 'pages#account'
+  match '/cart',  :to => 'pages#cart'
+  match '/listing',  :to => 'pages#listing'
+  match '/detail',  :to => 'pages#detail'
+  match '/index',  :to => 'products#index'
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
